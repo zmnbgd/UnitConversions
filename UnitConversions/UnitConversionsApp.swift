80670038c0e8411f17d30254bfde6514f6cb7dd5
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-12 18:06:44 +0100

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Marko Zivanovic on 12.12.24..
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
