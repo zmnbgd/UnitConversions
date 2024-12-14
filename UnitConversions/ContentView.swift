72401deee8c2d4593afbabcc2e734fc4dbe4bff2
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-14 22:08:06 +0100

    Challenge day 3

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -9,15 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var inputUnit = 0.0
+    @State private var outputUnit = 0.0
     
     let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
-    @State private var selectedTemperatureUnits = "Celsius"
+    @State private var selectedInputTemperatureUnits = "Celsius"
+    @State private var selectedOutputTemperatureUnit = "Fahrenheit"
+    
     var body: some View {
         
         NavigationStack {
             Form {
                 Section("Unit input") {
-                    Picker("Choose a unit of measure", selection: $selectedTemperatureUnits) {
+                    Picker("Choose a unit of measure", selection: $selectedInputTemperatureUnits) {
                         ForEach(temperatureUnits, id: \.self) {
                             Text($0)
                         }
@@ -25,15 +29,20 @@ struct ContentView: View {
                 }
                 
                 Section("Enter a input unit value") {
-                    
+                    TextField("Input value", value: $inputUnit, format: .number)
+                        .keyboardType(.decimalPad)
                 }
                 
                 Section("Output unit") {
-                    
+                    Picker("Choose a unit of measure", selection: $selectedOutputTemperatureUnit) {
+                        ForEach(temperatureUnits, id: \.self) {
+                            Text($0)
+                        }
+                    }
                 }
                 
                 Section("Output of the conversion") {
-                    
+                    TextField("Output value", value: $outputUnit, format: .number)
                 }
             }
             .navigationTitle("Unit Conversions")
@@ -45,3 +54,6 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+
